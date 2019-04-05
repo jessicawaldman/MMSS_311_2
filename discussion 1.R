c544e22f98b93360bc68456fbe76f183ae3d1b50
Jessica Waldman <jessicawaldman2021@u.northwestern.edu>
2019-04-05 15:26:10 -0500

    Update
    Co-Authored-By: Napon Jatusripitak <naponjatusripitak2021@u.northwestern.edu>

--- discussion 1.R
+++ discussion 1.R
@@ -1,5 +1,5 @@
 setwd("~/Documents/GitHub/MMSS_311_2")
-install.packages(("dplyr", "ggplot2", "stringr", "lubridate"))
+install.packages(c("dplyr", "ggplot2", "stringr","lubridate"))
 
 qog <-read.csv("http://www.qogdata.pol.gu.se/data/qog_std_cs_jan19.csv")
 print(dim(qog))
